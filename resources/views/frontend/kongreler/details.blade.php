a0ff3b9f37661661f4b136ec877b9ef352d8dad1
yuzgec <olcayy@gmail.com>
2023-09-21 12:23:52 +0300

    Fix

--- resources/views/frontend/kongreler/details.blade.php
+++ resources/views/frontend/kongreler/details.blade.php
@@ -6,7 +6,7 @@
         <div class="container container-xl-custom">
             <div class="row">
                 <div class="col-md-12 align-self-center p-static order-2 text-center">
-                    <h1 class="font-weight-bold text-dark">{{ $Detay->title }}</h1>
+                    <h1 class="font-weight-bold text-dark">SASDER {{ $Detay->title }}</h1>
                 </div>
                 <div class="col-md-12 align-self-center order-1">
                     <ul class="breadcrumb d-block text-center">
